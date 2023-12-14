c8adb33ecf22cee0af7583f573f087bdd55ed944
tb <tb@home-klick.de>
2023-12-14 11:19:38 +0100

    init

---
+++ main.py
@@ -0,0 +1,23 @@
+import datetime
+
+from pymongo import MongoClient
+username = "admin"
+password = "password"
+client = MongoClient("localhost", 27018, username=username, password=password)
+
+db = client.test_database
+
+
+post = {
+    "author": "Mike",
+    "text": "My first blog post!",
+    "tags": ["mongodb", "python", "pymongo"],
+    "date": datetime.datetime.now(tz=datetime.timezone.utc),
+}
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+all_posts = posts.find()
+
+# Print each post
+for post in all_posts:
+    print(post)
